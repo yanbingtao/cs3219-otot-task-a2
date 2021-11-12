8212b8c247862313acb74abbf175732d5bcaf93d
yanbingtao <yanbingtao963@Gmail.com>
2021-11-12 10:09:46 +0800

    Deploying Apps on Kubernetes

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello world for otot task a2");
+});
+
+app.listen(3000, () => console.log("server started on port 3000"));
